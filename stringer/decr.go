4ea9f96f764c78f78f364bad1528980e3c554b7b
witwoywhy <but.nuttawit.dev@gmail.com>
2022-04-27 13:58:59 +0700

    refactor: decr

--- stringer/decr.go
+++ stringer/decr.go
@@ -2,36 +2,24 @@ package stringer
 
 import (
 	"strconv"
-	"unicode/utf8"
 )
 
 func (s *String) decrBy(key string, decr int) (int, error) {
-	value := s.Get(key)
-	if utf8.RuneCountInString(value) == 0 {
-		if decr != 0 {
-			n := 0
-			n -= decr
-
-			str := strconv.Itoa(n)
-			s.Set(key, str, 0)
-			return n, nil
+	var num int
+	r, ok := s.get(key)
+	if ok {
+		n, err := strconv.Atoi(r.value)
+		if err != nil {
+			return 0, err
 		}
-
-		s.Set(key, "0", 0)
-		return 0, nil
+		num = n
 	}
 
-	n, err := strconv.Atoi(value)
-	if err != nil {
-		return 0, err
-	}
-
-	n -= decr
-
-	str := strconv.Itoa(n)
+	num -= decr
+	
+	str := strconv.Itoa(num)
 	s.Set(key, str, 0)
-
-	return n, nil
+	return num, nil
 }
 
 func (s *String) Decr(key string) (int, error) {
